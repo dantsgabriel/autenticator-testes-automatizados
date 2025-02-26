ad7a8b7373616f829f3ac0cad18865eb70d0a89b
Gabriel Dantas <gabriel_ad12@hotmail.com>
2025-02-26 16:49:55 -0300

    primeiro commit

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+class AccountsViewTest(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="gabriel",
+            password="teste123"
+        )
+
+    def test_login_sucesso(self):
+        response = self.client.post(reverse('login'), {
+            'username': 'gabriel',
+            'password': 'teste123'
+        })
+        self.assertRedirects(response, reverse('dashboard'))
+
+    def test_login_falha(self):
+        response = self.client.post(reverse('login'), {
+            'username': 'gabriel',
+            'password': '123teste'
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Credenciais inválidas")        
+
+    def test_homepage_requer_login(self):
+        response = self.client.get(reverse('dashboard'))
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(response.url.startswith(reverse('login')))        
